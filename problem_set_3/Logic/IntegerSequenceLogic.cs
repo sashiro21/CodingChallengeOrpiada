c27a818ecdb15086e50f74f43bcd2e1cc1082616
sashiro21 <enzosieghart@gmail.com>
2024-12-17 08:37:51 +0800

    Commit initial code for set problem 3

---
+++ problem_set_3/Logic/IntegerSequenceLogic.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_set_3.Logic;
+
+internal static class IntegerSequenceLogic
+{
+    internal static int LongestIncreasingSequenceCount(int[] numbers)
+    {
+        int result = 0;
+        List<List<int>> data = new();
+
+        foreach (int number in numbers)
+        {
+            if(data.Any())
+            {
+                foreach (var item in data)
+                {
+                    
+                }
+            }
+            else
+            {
+            }
+        }
+
+        return result;
+    }
+
+    internal static int LongestIncreasingSequenceCountV2(int[] numbers)
+    {
+        int result = 0;
+        int[][] data = new int[numbers.Length][];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            data[i][0] = numbers[i];
+
+
+
+        }
+
+        return result;
+    }
+}
